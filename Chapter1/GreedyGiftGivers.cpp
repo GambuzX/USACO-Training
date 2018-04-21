6e50d96207ca06c6218a64ebe3a102133a8a8c29
Mário Gil <mg_mesquita@hotmail.com>
2018-04-21 14:06:23 +0100

    Finished GreedyGiftGivers

--- Chapter1/GreedyGiftGivers.cpp
+++ Chapter1/GreedyGiftGivers.cpp
@@ -47,27 +47,52 @@ int main()
 	ifstream input("gift1.in");
 	ofstream output("gift1.out");
 
-	int np;
-	input >> np;
-	input.ignore(1000, '\n');
+	string np;
+	getline(input, np);
 
 	vector<Friend> friends;
-	for (int i = 0; i < np; i++)
+	for (int i = 0; i < stoi(np); i++)
 	{
 		string currentName;
 		getline(input, currentName);
 		friends.push_back(Friend(currentName));
 	}
 
-	string line;
-	while (getline(input, line))
+	string giver;
+	while (getline(input, giver))
 	{
+		string line;
+		getline(input, line);
 		int spaceLocation = line.find_first_of(' ');
 		string totalMoney = line.substr(0, spaceLocation);
-		string numberOfPersons = line.substr(spaceLocation, line.length() - spaceLocation); //TODO VERIFY THIS
+		string numberOfPersons = line.substr(spaceLocation+1, line.length() - spaceLocation+1); //TODO VERIFY THIS
+		int giftedMoney = stoi(totalMoney);
+		int nPersons = stoi(numberOfPersons);
+		int moneyFraction = nPersons == 0 ? 0 : giftedMoney / nPersons;
+		int remainderMoney = nPersons == 0 ? 0 : giftedMoney % nPersons;
 
+		for (int i = 0; i < friends.size(); i++)
+		{
+			if (friends.at(i).getName() == giver)
+				friends.at(i).updateBalance(-giftedMoney + remainderMoney);
+		}
+
+		for (int j = 0; j < nPersons; j++)
+		{
+			string receiver;
+			getline(input, receiver);
+			for (int i = 0; i < friends.size(); i++)
+			{
+				if (friends.at(i).getName() == receiver)
+					friends.at(i).updateBalance(moneyFraction);
+			}
+		}
 	}
-	
 
-
+	//OUTPUT
+	for (int i = 0; i < friends.size(); i++)
+	{
+		output << friends.at(i).getName() << ' ' << friends.at(i).getBalance() << '\n';
+	}
+	output.close();
 }
